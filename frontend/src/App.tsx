3e98c7cbe516adaa85782546786913482688dc67
sascha horn <saschahorn83@gmail.com>
2023-12-15 17:06:36 +0100

    fetch and display popular movie refactor component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,24 +8,36 @@ import {Route, Routes} from "react-router-dom";
 
 export default function App() {
 
-    const baseURL: string = "/movies/popular"
+
     const [popularMovies, setPopularMovies] = useState<Movie[]>([])
+    const [newcomerMovies, setNewcomerMovies] = useState<Movie[]>([])
 
     useEffect(() => {
-        fetchData()
+        fetchDataPopular()
+        fetchDataNewcomer()
     }, []);
 
-    const fetchData = () => {
-        axios.get(baseURL)
+    const fetchDataPopular = () => {
+        axios.get("/movies/popular")
             .then(response => setPopularMovies(response.data))
             .catch(error => {
                 console.error("error information: ", error)
             })
     }
 
+    const fetchDataNewcomer = () => {
+        axios.get("/movies/newcomer")
+            .then(response => setNewcomerMovies(response.data))
+            .catch(error => {
+                console.error("error information: ", error)
+            })
+    }
+
+
+
     return (
         <Routes>
-            <Route path={"/home"} element={<Home movies={popularMovies}/>}/>
+            <Route path={"/home"} element={<Home moviesPopular={popularMovies} moviesNewcomer={newcomerMovies}/>}/>
         </Routes>
     )
 }
