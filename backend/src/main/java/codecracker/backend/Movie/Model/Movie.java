eb3fe72a5aaff6ff7c436edfc78f55b51fcccdd4
nils <nilsotto@gmx.de>
2023-12-18 14:42:33 +0100

    modify Movie

--- backend/src/main/java/codecracker/backend/Movie/Model/Movie.java
+++ backend/src/main/java/codecracker/backend/Movie/Model/Movie.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,6 +17,7 @@ public class Movie {
     private String poster_path;
     private String release_date;
     private String title;
+    private List<MovieGenre> genres;
 }
 
 
