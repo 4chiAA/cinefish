300532a88cc85f78e1ddc2a02a7ea9b42a170b3d
sascha horn <saschahorn83@gmail.com>
2023-12-19 15:21:51 +0100

    start styling detail page

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -1,4 +1,5 @@
 import {Genre, MovieDetail} from "../Movie.ts";
+import styled from "styled-components";
 
 type DetailPageProps = {
     movieDetails: MovieDetail | undefined | null
@@ -16,21 +17,70 @@ export default function DetailPage(props: DetailPageProps) {
 
     return (
         <>
-            <section>
-                <img src={posterImageUrl + props.movieDetails.poster_path}
-                     alt={"Poster of " + props.movieDetails.original_title}/>
-                <p>{props.movieDetails.original_title}</p>
-                {props.movieDetails.genres?.map((genre: Genre) => (
-                    <p key={genre.id}>{genre.name}</p>
-                ))}
+            <StyledContainerSection>
+                <StyledImageWrapper>
+                    <StyledImage src={posterImageUrl + props.movieDetails.poster_path}
+                                 alt={"Poster of " + props.movieDetails.original_title}/>
+                </StyledImageWrapper>
+                <StyledInfoTitleSection>
+                    <StyledTitleParagraph>{props.movieDetails.original_title}</StyledTitleParagraph>
+                    <StyledInfoSection>
+                        <p>Genre</p>
+                        <ul>
+                            {props.movieDetails.genres?.map((genre: Genre) => (
 
-                <p>{props.movieDetails.runtime}</p>
-                <p>{props.movieDetails.release_date}</p>
-                <p>{props.movieDetails.vote_average}</p>
-            </section>
+                                <li key={genre.id}>{genre.name}</li>
+                            ))}
+                        </ul>
+                        <p>Runtime</p>
+                        <p>       {props.movieDetails.runtime}</p>
+                        <p> Release Date</p>
+                        <p>{props.movieDetails.release_date}</p>
+                        <p>Score</p>
+                        <p>                        {props.movieDetails.vote_average}</p>
+                    </StyledInfoSection>
+                </StyledInfoTitleSection>
+            </StyledContainerSection>
+
 
             <p>{props.movieDetails.overview}</p>
-
         </>
     )
 }
+
+const StyledContainerSection = styled.section`
+    display: flex;
+    flex-direction: row;
+    margin: 10px;
+height: 300px;
+`
+const StyledInfoSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    max-height: 80%;
+    align-items: center;
+    font-size: 0.4em
+`
+const StyledTitleParagraph = styled.p`
+    display: flex;
+    text-align: center;
+`
+const StyledInfoTitleSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+`
+
+const StyledImageWrapper = styled.div`
+    width: 50%;
+    overflow: hidden;
+`
+
+const StyledImage = styled.img`
+    width: 100%;
+    height: auto;
+    display: block;
+
+`
+
